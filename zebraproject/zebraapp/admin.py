1ae746fe7ae062c64adc92d426b94f544de2e556
svdoit <doemily@naver.com>
2020-11-17 02:13:16 +0900

    마이페이지 myItem등록 기능, myPage.html에서의 확인 기능 구현

--- zebraproject/zebraapp/admin.py
+++ zebraproject/zebraapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Product , ChildProduct 
+from .models import Category, Product , ChildProduct, MyItem
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(ChildProduct)
+admin.site.register(MyItem)
