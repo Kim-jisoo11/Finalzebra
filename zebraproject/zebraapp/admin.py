363e5fadf4cffc8850f41ce6958c5fdc4bc3012d
sungmin <icechocola05@gmail.com>
2020-11-19 14:21:24 +0900

    Product 모델에 ChildProduct 모델 상속
    -Product에서 ChildProduct를 관리할 수 있음
    ex) 칫솔 안에 더피커 칫솔, 제로웨이스트 칫솔

--- zebraproject/zebraapp/admin.py
+++ zebraproject/zebraapp/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Category, Product , ChildProduct 
 # Register your models here.
 
+class ChildProductInline(admin.TabularInline):
+    model = ChildProduct
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ChildProductInline, ]
+
 admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(ChildProduct)
+admin.site.register(Product, ProductAdmin)
